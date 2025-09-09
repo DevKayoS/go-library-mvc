8d3549d23073134edc332904c64ca1a526c4222d
Kayo <kayoribeirodev@gmail.com>
2025-09-09 19:00:41 -0300

    feat: fazendo a controller do de users

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"log"
 
+	"github.com/DevKayoS/go-library-mvc/internal/users/controllers"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
-	_ = router
+	userController := controllers.NewUserController()
+	userController.RegisterRoutes(router)
 
 	if err := router.Run(":8080"); err != nil {
 		log.Fatal(err)
