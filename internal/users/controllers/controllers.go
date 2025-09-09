fc46d9496d58fe056f99802eb632bd865701e3fa
Kayo <kayoribeirodev@gmail.com>
2025-09-09 20:13:48 -0300

    feat: implementando as controllers e criando a interface do user service

--- internal/users/controllers/controllers.go
+++ internal/users/controllers/controllers.go
@@ -1,12 +1,21 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/DevKayoS/go-library-mvc/internal/users/models"
+	"github.com/gin-gonic/gin"
+)
 
 type UserController struct {
+	userService models.UserService
 }
 
-func NewUserController() *UserController {
-	return &UserController{}
+func NewUserController(userService models.UserService) *UserController {
+	return &UserController{
+		userService: userService,
+	}
 }
 
 func (c *UserController) RegisterRoutes(r *gin.Engine) {
@@ -21,19 +30,149 @@ func (c *UserController) RegisterRoutes(r *gin.Engine) {
 }
 
 func (c *UserController) CreateUser(ctx *gin.Context) {
+	var user models.User
+
+	if err := ctx.ShouldBindJSON(&user); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"status": false,
+			"code":   http.StatusBadRequest,
+			"error":  "Invalid Request Body",
+		})
+		return
+	}
+
+	err := c.userService.CreateUser(&user)
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"status": false,
+			"code":   http.StatusInternalServerError,
+			"error":  err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, gin.H{
+		"status":  true,
+		"code":    http.StatusCreated,
+		"message": "Created successfully",
+		"data":    user,
+	})
 }
 
 func (c *UserController) GetUser(ctx *gin.Context) {
+	id, err := strconv.ParseInt(ctx.Param("id"), 10, 64)
+
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"status": false,
+			"code":   http.StatusBadRequest,
+			"error":  "Invalid User Id",
+		})
+		return
+	}
+
+	user, err := c.userService.GetUser(id)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"status": false,
+			"code":   http.StatusInternalServerError,
+			"error":  err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"status": true,
+		"code":   http.StatusOK,
+		"data":   user,
+	})
 }
 
 func (c *UserController) GetAllUser(ctx *gin.Context) {
-	ctx.String(200, "FUNCIONOU")
+	user, err := c.userService.GetAllUser()
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"status": false,
+			"code":   http.StatusInternalServerError,
+			"error":  err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"status": true,
+		"code":   http.StatusOK,
+		"data":   user,
+	})
 }
 
 func (c *UserController) UpdateUser(ctx *gin.Context) {
+	id, err := strconv.ParseInt(ctx.Param("id"), 10, 64)
 
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"status": false,
+			"code":   http.StatusBadRequest,
+			"error":  "Invalid User Id",
+		})
+		return
+	}
+
+	var user models.User
+
+	if err := ctx.ShouldBindJSON(&user); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"status": false,
+			"code":   http.StatusBadRequest,
+			"error":  "Invalid Request Body",
+		})
+		return
+	}
+
+	err = c.userService.UpdateUser(id, &user)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"status": false,
+			"code":   http.StatusInternalServerError,
+			"error":  err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"status":  true,
+		"code":    http.StatusOK,
+		"message": "Updated successfully",
+	})
 }
 
 func (c *UserController) DeleteUser(ctx *gin.Context) {
+	id, err := strconv.ParseInt(ctx.Param("id"), 10, 64)
 
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"status": false,
+			"code":   http.StatusBadRequest,
+			"error":  "Invalid User Id",
+		})
+		return
+	}
+
+	err = c.userService.DeleteUser(id)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"status": false,
+			"code":   http.StatusInternalServerError,
+			"error":  err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"status":  true,
+		"code":    http.StatusOK,
+		"message": "User deleted",
+	})
 }
---
+++ internal/users/models/user.go
@@ -0,0 +1,11 @@
+package models
+
+import "time"
+
+type User struct {
+	Id        int64     `json:"id"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+}
