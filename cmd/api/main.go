e54a6275356cf33a9469e87ac866c8a801fb2c2d
Kayo <kayoribeirodev@gmail.com>
2025-09-09 19:13:29 -0300

    feat: criando a controller dos livros

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,14 +3,21 @@ package main
 import (
 	"log"
 
-	"github.com/DevKayoS/go-library-mvc/internal/users/controllers"
+	bookController "github.com/DevKayoS/go-library-mvc/internal/books/controllers"
+	userController "github.com/DevKayoS/go-library-mvc/internal/users/controllers"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
-	userController := controllers.NewUserController()
+
+	// User
+	userController := userController.NewUserController()
 	userController.RegisterRoutes(router)
+
+	// Book
+	bookController := bookController.NewBookController()
+	bookController.RegisterRoutes(router)
 
 	if err := router.Run(":8080"); err != nil {
 		log.Fatal(err)
